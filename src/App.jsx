fd97a54d336af034860ad6d357fc9b5be768b3f7
Adolfo0018 <adolfogk0018@gmail.com>
2022-10-06 17:22:05 -0500

    add a form Modal based on Modal components to register a expense

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import Modal from './components/Modal'
 
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+  const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+
+  const handleNuevoGasto = () =>{
+    setModal(true)
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500)
+  }
 
   return (
     <div>
@@ -18,9 +28,15 @@ function App() {
 
       {isValidPresupuesto && (
           <div className='nuevo-gasto'>
-            <img src={IconoNuevoGasto} alt="Icono Nuevo Gasto" />
+            <img 
+              src={IconoNuevoGasto} 
+              alt="Icono Nuevo Gasto" 
+              onClick={handleNuevoGasto}
+              />
           </div>
       )}
+
+      {modal && <Modal setModal={setModal} animarModal={animarModal} setAnimarModal={setAnimarModal}/>}
 
     </div>
 
